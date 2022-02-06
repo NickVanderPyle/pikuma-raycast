b95899c1114e18504cbdccec418e3034d12aa47f
nick <NickVanderPyle@users.noreply.github.com>
2022-02-05 19:36:23 -0600

    Refactor the wall projections out.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "map.h"
 #include "ray.h"
 #include "player.h"
+#include "wall.h"
 
 bool isGameRunning = false;
 int ticksOnLastFrame = 0;
@@ -74,50 +75,6 @@ void update() {
     castAllRays();
 }
 
-void renderWallProjection() {
-    for (int x = 0; x < NUM_RAYS; x++) {
-        ray_t ray = rays[x];
-        // corrects fisheye effect
-        float perpendicularDistance = ray.distance * cos(ray.rayAngle - player.rotationAngle);
-        float projectedWallHeight = (TILE_SIZE / perpendicularDistance) * DIST_PROJ_PLANE;
-
-        int wallStripHeight = projectedWallHeight;
-
-        int wallTopPixel = (WINDOW_HEIGHT / 2) - (wallStripHeight / 2);
-        wallTopPixel = wallTopPixel < 0 ? 0 : wallTopPixel;
-
-        int wallBottomPixel = (WINDOW_HEIGHT / 2) + (wallStripHeight / 2);
-        wallBottomPixel = wallBottomPixel > WINDOW_HEIGHT ? WINDOW_HEIGHT : wallBottomPixel;
-
-        //render ceiling color
-        for (int y = 0; y < wallTopPixel; y++) {
-            drawPixel(x, y, 0xFF333333);
-        }
-
-        //render wall color
-        int textureOffsetX = ray.wasHitVertical
-                             ? (int) ray.wallHitY % TILE_SIZE
-                             : (int) ray.wallHitX % TILE_SIZE;
-        int texNum = ray.wallHitContent - 1;
-        int textureWidth = wallTextures[texNum].width;
-        int textureHeight = wallTextures[texNum].height;
-        for (int y = wallTopPixel; y < wallBottomPixel; y++) {
-            //set color of wall pixel from color of texture pixel.
-            int distanceFromTop = y + (wallStripHeight / 2) - (WINDOW_HEIGHT / 2);
-            int textureOffsetY = distanceFromTop * ((float) textureHeight / wallStripHeight);
-            int textureOffset = (textureWidth * textureOffsetY) + textureOffsetX;
-            uint32_t texelColor = wallTextures[texNum].texture_buffer[textureOffset];
-
-            drawPixel(x, y, texelColor);
-        }
-
-        //render floor color
-        for (int y = wallBottomPixel; y < WINDOW_HEIGHT; y++) {
-            drawPixel(x, y, 0xFF777777);
-        }
-    }
-}
-
 void render() {
     clearColorBuffer(0xFF000000);
 
@@ -149,5 +106,5 @@ int main() {
 
     releaseResources();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
---
+++ src/wall.c
@@ -0,0 +1,50 @@
+#include "wall.h"
+#include "defs.h"
+#include "ray.h"
+#include "player.h"
+#include "graphics.h"
+#include "textures.h"
+
+void renderWallProjection(void) {
+    for (int x = 0; x < NUM_RAYS; x++) {
+        ray_t ray = rays[x];
+        // corrects fisheye effect
+        float perpendicularDistance = ray.distance * cos(ray.rayAngle - player.rotationAngle);
+        float projectedWallHeight = (TILE_SIZE / perpendicularDistance) * DIST_PROJ_PLANE;
+
+        int wallStripHeight = projectedWallHeight;
+
+        int wallTopPixel = (WINDOW_HEIGHT / 2) - (wallStripHeight / 2);
+        wallTopPixel = wallTopPixel < 0 ? 0 : wallTopPixel;
+
+        int wallBottomPixel = (WINDOW_HEIGHT / 2) + (wallStripHeight / 2);
+        wallBottomPixel = wallBottomPixel > WINDOW_HEIGHT ? WINDOW_HEIGHT : wallBottomPixel;
+
+        //render ceiling color
+        for (int y = 0; y < wallTopPixel; y++) {
+            drawPixel(x, y, 0xFF333333);
+        }
+
+        //render wall color
+        int textureOffsetX = ray.wasHitVertical
+                             ? (int) ray.wallHitY % TILE_SIZE
+                             : (int) ray.wallHitX % TILE_SIZE;
+        int texNum = ray.wallHitContent - 1;
+        int textureWidth = wallTextures[texNum].width;
+        int textureHeight = wallTextures[texNum].height;
+        for (int y = wallTopPixel; y < wallBottomPixel; y++) {
+            //set color of wall pixel from color of texture pixel.
+            int distanceFromTop = y + (wallStripHeight / 2) - (WINDOW_HEIGHT / 2);
+            int textureOffsetY = distanceFromTop * ((float) textureHeight / wallStripHeight);
+            int textureOffset = (textureWidth * textureOffsetY) + textureOffsetX;
+            uint32_t texelColor = wallTextures[texNum].texture_buffer[textureOffset];
+
+            drawPixel(x, y, texelColor);
+        }
+
+        //render floor color
+        for (int y = wallBottomPixel; y < WINDOW_HEIGHT; y++) {
+            drawPixel(x, y, 0xFF777777);
+        }
+    }
+}
